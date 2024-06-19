ac27dec4535dc8f4ac3a03943568a9287d464e4e
Steward Lumowa <stewardyohanes29@gmail.com>
2024-06-19 08:53:52 +0800

    deploy to vercel

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
