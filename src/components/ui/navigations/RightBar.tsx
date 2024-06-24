7b1b2b20be4ad2448a55d67a05d4fcbbdb7abefe
Steward Lumowa <stewardyohanes29@gmail.com>
2024-06-24 10:10:02 +0800

    fix: refactor code in right bar

--- src/components/ui/navigations/RightBar.tsx
+++ src/components/ui/navigations/RightBar.tsx
@@ -17,8 +17,10 @@ import Suggestion from "../Suggestion";
 import { useQuery } from "@tanstack/react-query";
 import useFetchProfile from "../../../hooks/useFetchProfile";
 import { AuthUser } from "../../../features/auth/types/auth.type";
+import { useLocation } from "react-router-dom";
 
 export default function RightBar() {
+  const location = useLocation();
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { data, isPending } = useFetchSuggestion();
   const { data: authUser } = useQuery<AuthUser>({ queryKey: ["authUser"] });
@@ -33,94 +35,100 @@ export default function RightBar() {
       p={"20px"}
       width={"50%"}
     >
-      <Card title="My Profile">
-        {isPending ? (
-          <Spinner color="blue.500" alignSelf={"center"} />
-        ) : (
-          <Box position={"relative"} display={"flex"} flexDirection={"column"}>
-            <Image
-              src={profile?.coverImage}
-              alt={"logo"}
-              width={"100%"}
-              height={"200px"}
-              objectFit={"cover"}
-            />
-
-            <Box position={"absolute"} top={"150px"} left={"20px"}>
-              <Avatar
-                boxSize={"100px"}
-                border={"2px solid black"}
-                name={profile?.username}
-                src={profile?.photoProfile}
-              />
-            </Box>
-
-            <Box display={"flex"} flexDirection={"column"} mb={"10px"}>
-              <Button
-                width={"110px"}
-                size={"sm"}
-                border={"1px solid"}
-                borderColor={"gray.500"}
-                borderRadius={"full"}
-                bg={"none"}
-                color={"white"}
-                alignSelf={"flex-end"}
-                mt={"10px"}
-                onClick={onOpen}
-              >
-                Edit Profile
-              </Button>
-
-              <ModalProfile
-                isOpen={isOpen}
-                onClose={onClose}
-                size={{
-                  base: "full",
-                  sm: "full",
-                  md: "lg",
-                  lg: "xl",
-                  xl: "2xl",
-                }}
+      {location.pathname !== "/profile" && (
+        <Card title="My Profile">
+          {isPending ? (
+            <Spinner color="blue.500" alignSelf={"center"} />
+          ) : (
+            <Box
+              position={"relative"}
+              display={"flex"}
+              flexDirection={"column"}
+            >
+              <Image
+                src={profile?.coverImage}
+                alt={"logo"}
+                width={"100%"}
+                height={"200px"}
+                objectFit={"cover"}
               />
 
-              <Box mt={"20px"} mb={"10px"}>
-                <Heading size={"md"}>{profile?.fullName}</Heading>
-                <Text color={"gray.500"} fontSize={"sm"} mb={"10px"}>
-                  @{profile?.username}
-                </Text>
-                <Text>{profile?.bio}</Text>
-                <Text
-                  as={"span"}
-                  color={"gray.500"}
-                  fontSize={"sm"}
-                  mt={"15px"}
-                  display={"flex"}
-                  alignItems={"center"}
-                  gap={"5px"}
+              <Box position={"absolute"} top={"150px"} left={"20px"}>
+                <Avatar
+                  boxSize={"100px"}
+                  border={"2px solid black"}
+                  name={profile?.username}
+                  src={profile?.photoProfile}
+                />
+              </Box>
+
+              <Box display={"flex"} flexDirection={"column"} mb={"10px"}>
+                <Button
+                  width={"110px"}
+                  size={"sm"}
+                  border={"1px solid"}
+                  borderColor={"gray.500"}
+                  borderRadius={"full"}
+                  bg={"none"}
+                  color={"white"}
+                  alignSelf={"flex-end"}
+                  mt={"10px"}
+                  onClick={onOpen}
                 >
-                  <CalendarDays size={"1rem"} color={"gray"} />
-                  Joined {moment(profile?.createdAt).format("LL")}
-                </Text>
-              </Box>
+                  Edit Profile
+                </Button>
 
-              <Box display={"flex"} gap={"20px"}>
-                <Text fontSize={"sm"} color={"gray.500"}>
-                  <Text fontWeight={"bold"} as={"span"} color={"white"}>
-                    {profile?.followings}
-                  </Text>{" "}
-                  Following
-                </Text>
-                <Text fontSize={"sm"} color={"gray.500"}>
-                  <Text fontWeight={"bold"} as={"span"} color={"white"}>
-                    {profile?.followers}
-                  </Text>{" "}
-                  Followers
-                </Text>
+                <ModalProfile
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  size={{
+                    base: "full",
+                    sm: "full",
+                    md: "lg",
+                    lg: "xl",
+                    xl: "2xl",
+                  }}
+                />
+
+                <Box mt={"20px"} mb={"10px"}>
+                  <Heading size={"md"}>{profile?.fullName}</Heading>
+                  <Text color={"gray.500"} fontSize={"sm"} mb={"10px"}>
+                    @{profile?.username}
+                  </Text>
+                  <Text>{profile?.bio}</Text>
+                  <Text
+                    as={"span"}
+                    color={"gray.500"}
+                    fontSize={"sm"}
+                    mt={"15px"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    gap={"5px"}
+                  >
+                    <CalendarDays size={"1rem"} color={"gray"} />
+                    Joined {moment(profile?.createdAt).format("LL")}
+                  </Text>
+                </Box>
+
+                <Box display={"flex"} gap={"20px"}>
+                  <Text fontSize={"sm"} color={"gray.500"}>
+                    <Text fontWeight={"bold"} as={"span"} color={"white"}>
+                      {profile?.followings}
+                    </Text>{" "}
+                    Following
+                  </Text>
+                  <Text fontSize={"sm"} color={"gray.500"}>
+                    <Text fontWeight={"bold"} as={"span"} color={"white"}>
+                      {profile?.followers}
+                    </Text>{" "}
+                    Followers
+                  </Text>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        )}
-      </Card>
+          )}
+        </Card>
+      )}
       <Card title="Suggestion For You">
         {isPending ? (
           <Spinner color="blue.500" alignSelf={"center"} />
